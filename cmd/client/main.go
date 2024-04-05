0de790ec0efc9aa21bb341afaf65404f51d98cca
Mr.Pavidl <pavelchichev@mail.ru>
2024-04-06 02:38:58 +0300

    Changes by densh

---
+++ cmd/client/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"space-go/internal/client"
+)
+
+func main() {
+	baseURL := "https://datsedenspace.datsteam.dev"
+	token := "660c35366abee660c35366abf1"
+
+	ctx := context.Background()
+	apiClient := client.NewClient(baseURL, token)
+
+	player, err := apiClient.GetUniverse(ctx)
+	if err != nil {
+		fmt.Printf("Error getting universe: %v\n", err)
+		return
+	}
+	fmt.Printf("Player: %v\n", player)
+}
